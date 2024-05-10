74a272370dfa3e78e957295af6539686e78b5e53
rapha1004 <106275964+rapha1004@users.noreply.github.com>
2024-05-10 17:25:30 +0000

    zz

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let startBtn = document.getElementById('start');
 let carCount = 0;
 let intervalId; 
-
+let spawnLocations = [1, 2, 3, 4, 5, 6, 7, 8]
 
 startBtn.addEventListener('click', () => {
     startBtn.style.visibility = "hidden";
@@ -18,33 +18,89 @@ function genRndCar() {
         }
         let car = document.createElement('div');
         car.classList.add('car');
-        car.style.width = "50px";
-        car.style.height = "20px";
-        car.style.margin = "30px";
+        car.style.width = "70px";
+        car.style.height = "40px";
         car.style.position = "absolute"
-        rotate = Math.round(Math.random());
         //car.style.transform = "rotate(90deg)";
-        const spawn = Math.floor(Math.random() * 8);
-        if (spawn == 1) {
-            car.style.left = "10%"
-            car.style.top = "30%"
-        }else if (spawn == 2) {
-            car.style.left = "10%"
-            car.style.top = "60%"
-        }else if (spawn == 3) {
-            car.style.left = "30%"
+        let spawnIndex = Math.floor(Math.random() * spawnLocations.length); // Choix aléatoire d'un emplacement de spawn disponible
+        const spawn = spawnLocations[spawnIndex];
+        spawnLocations.splice(spawnIndex, 1);
+        if (spawn === 1) {
+            car.style.left = "50px"
+            car.style.top = "415px"
+            car.classList.add('1')
+        }else if (spawn === 2) {
+            car.style.left = "50px"
+            car.style.top = "560px"
+            car.classList.add('2')
+        }else if (spawn === 3) {
+            car.style.transform = "rotate(90deg)";
+            car.style.left = "750px"
             car.style.top = "90%"
+            car.classList.add('3')
+        }else if (spawn === 4) {
+            car.style.transform = "rotate(90deg)";
+            car.style.left = "1050px"
+            car.style.top = "90%"
+            car.classList.add('4')
+        }else if (spawn === 5) {
+            car.style.left = "90%"
+            car.style.top = "415px"
+            car.classList.add('5')
+        }else if (spawn === 6) {
+            car.style.left = "90%"
+            car.style.top = "560px"
+            car.classList.add('6')
+        }else if (spawn === 7) {
+            car.style.transform = "rotate(90deg)";
+            car.style.left = "750px"
+            car.style.top = "10%"
+            car.classList.add('7')
+        }else if (spawn === 8) {
+                car.style.transform = "rotate(90deg)";
+                car.style.left = "1050px"
+                car.style.top = "10%"
+                car.classList.add('8')
         }
         var colorList = ["#ff0000", "#00ff00", "#0000ff", "#ffff00", "#ff00ff", "#00ffff"];
         var randomIndex = Math.floor(Math.random() * colorList.length)
         var randomColor = colorList[randomIndex]
         car.style.background = randomColor
         car.addEventListener('click', (event) => {
-            event.target.remove()
-            carCount--; 
+            let currentCar = event.target
+            currentCar.classList
+            if (currentCar.classList.contains('1') || currentCar.classList.contains('2')){
+                var carPos = currentCar.getBoundingClientRect();
+                var randomX = window.innerWidth + 200;
+                currentCar.style.left = randomX + "px";
+            }else if(currentCar.classList.contains('3') || currentCar.classList.contains('4')){
+                var carPos = currentCar.getBoundingClientRect();
+                var randomY = window.innerHeight + 200;
+                currentCar.style.top = "-" + randomY + "px";
+            }else if(currentCar.classList.contains('5') || currentCar.classList.contains('6')){
+                var carPos = currentCar.getBoundingClientRect();
+                var randomX = window.innerWidth + 200;
+                currentCar.style.left = "-" + randomX + "px";
+            }else{
+                var carPos = car.getBoundingClientRect();
+                var randomY = window.innerHeight + 200;
+                currentCar.style.top = randomY + "px";  
+            }
+            rectangleBleu.style.top = randomY + "px";
             document.querySelector('h3').innerText = carCount
         });
         document.body.appendChild(car);
         carCount++; 
     }, 200);
+}
+setInterval(() => {
+    delCar()
+}, 200);
+
+function delCar(event) {
+    if (car.style.top >= innerHeight || car.style.top >= innerHeight - innerHeight) {
+        event.target.remove()
+    }else if(car.style.left >= innerWidth || car.style.left >= innerWidth - innerWidth){
+        event.target.remove()
+    }
 }
